028e5c84ddc4317e7106de65de658692bae332b0
Nick Halper <nhalper@blackrockmicro.com>
2017-01-05 13:01:02 -0700

    No Longer Can Select Upsampled NSP

--- CorrectDriftWithoutSignal.m
+++ CorrectDriftWithoutSignal.m
@@ -1,4 +1,4 @@
-function CorrectDriftWithoutSignal (ForceHFMethod,ForceLFMethod,ForcePredictionMethod,SelectUpsampleNSP) 
+function CorrectDriftWithoutSignal (ForceHFMethod,ForceLFMethod,ForcePredictionMethod) 
 %% 
 % 
 % 
